57c4be0f15ddb5e92c23b88721d2a84905e9f2cd
safae Fanina <safae.fanina@gmail.com>
2021-04-01 15:57:38 +0200

    oAdd travis file fourth

--- hello.py
+++ hello.py
@@ -3,11 +3,11 @@
 from flask import Flask
 app = Flask(__name__)
 @app.route('/')
-"""" Rien """"
+""" Rien """
 def index():
     return "Hello world !"
 @app.route('/hello/fanina')
-"""" Rien """"
+""" Rien """
 def optional():
     return "Hello "
 if __name__ == "__main__":
