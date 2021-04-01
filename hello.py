157cf88b2665fed0b1a25f27b7bf5cb4237e51db
safae Fanina <safae.fanina@gmail.com>
2021-04-01 15:50:21 +0200

    oAdd travis file fourth

--- hello.py
+++ hello.py
@@ -1,9 +1,13 @@
+ """This script prompts a user to enter a message to encode or decode
+ using a classic Caesar shift substitution (3 letter shift)"""
 from flask import Flask
 app = Flask(__name__)
 @app.route('/')
+"""" Rien """"
 def index():
     return "Hello world !"
 @app.route('/hello/fanina')
+"""" Rien """"
 def optional():
     return "Hello "
 if __name__ == "__main__":
