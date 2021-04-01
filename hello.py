1f152347f9f002b1cfc4705c12917708531e9a23
safae Fanina <safae.fanina@gmail.com>
2021-04-01 15:54:15 +0200

    oAdd travis file fourth

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
- """This script prompts a user to enter a message to encode or decode
+"""This script prompts a user to enter a message to encode or decode
  using a classic Caesar shift substitution (3 letter shift)"""
 from flask import Flask
 app = Flask(__name__)
@@ -12,4 +12,3 @@ def optional():
     return "Hello "
 if __name__ == "__main__":
     app.run()
-    
